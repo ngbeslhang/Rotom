84a2f3c1ef090069cf04d800b76734edf178fc3b
ngbeslhang <ngbeslhang@gmail.com>
2016-11-09 23:45:55 +0800

    __init__() completed

--- ext/db_couchdb.py
+++ ext/db_couchdb.py
@@ -6,5 +6,21 @@ import couchdb
 required = ["couchdb"]
 
 class DB:
-    def __init__(self, host, port, name, ):
-        self._db = "placeholder" # couchdb.Server
+    def __init__(self, host, port, name, user=None, passwd=None):
+        """
+        Connects to the server and create a database for the bot.
+        """
+        if user and passwd != None:
+            url = "https://{}:{}@{}:{}".format(user, passwd, host, port)
+        elif user != None:
+            url = "https://{}@{}:{}".format(user, host, port)
+        else:
+            url = "https://{}:{}".format(host, port)
+
+        self._server = couchdb.Server(url)
+        del url
+
+        try:
+            self._db = self._server.create(name)
+        except couchdb.PreconditionFailed:
+            self._db = self._server[name]
