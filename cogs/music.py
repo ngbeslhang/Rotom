17e560d1663fae2d79c80c3731e37d8c0728131a
ngbeslhang <ngbeslhang@gmail.com>
2016-11-11 19:11:20 +0800

    Added MusicEntry and Queue classes

--- cogs/music.py
+++ cogs/music.py
@@ -1,6 +1,32 @@
 """Music feature for Rotom"""
+import asyncio
 #import discord
 from discord.ext.commands import command
+
+
+class MusicEntry:
+    """Music entry class for Queue class"""
+    def __init__(self, player, mode):
+        """Initialize with player and mode.
+
+        player    - discord.py's built-in player coroutine
+                    If the URL provided is a playlist, a list of players instead.
+                    Considering setting a limit per playlist
+                    The skip command, if used without "playlist" or "pl" or "list",
+                    only skips to the next player in the list, otherwise the whole playlist
+                    will be skipped instead.
+        mode: str - Player mode, e.g. yt, sc, twitch
+        """
+        self.player = player
+        self.mode = mode
+
+class Queue:
+    """Queue class for _queue"""
+    def __init__(self, text_channel):
+        """
+        """
+        self.text_channel = text_channel
+        #self._queue = 
 
 class Music:
     """
@@ -8,5 +34,7 @@ class Music:
     **NOTE**: Must be modified to be compactible with other discord.ext bots.
     Rotom-exclusive parts:
     - Database"""
+
     def __init__(self, bot):
         self.bot = bot
+        self._queue = {}
