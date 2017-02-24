65fa0f07f8ac90056c9d25af4ce1a2037c938e87
ngbeslhang <ngbeslhang@gmail.com>
2017-02-25 05:44:08 +0800

    Initial commit

---
+++ cogs/utils/checks.py
@@ -0,0 +1,8 @@
+"""Some of them shamelessly copied from RoboDanny :Thonkang:"""
+from discord.ext import commands
+
+def is_owner_check(msg, owner):
+    return message.author.id in owner
+
+def is_owner():
+    return commands.check(lambda ctx: is_owner_check(ctx.message, ctx.bot.owner))
