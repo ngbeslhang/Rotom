299c164ffe4899b105ad34ef17e90b2283808986
ngbeslhang <ngbeslhang@gmail.com>
2017-03-04 04:41:14 +0800

    Added selfbot ID check

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,8 +1,8 @@
 """Some of them shamelessly copied from RoboDanny :Thonkang:"""
 from discord.ext import commands
 
-def is_owner_check(msg, owner):
-    return msg.author.id in owner
+def is_owner_check(ctx):
+    return ctx.message.author.id in (ctx.bot.owner, ctx.bot.user.id)
 
 def is_owner():
-    return commands.check(lambda ctx: is_owner_check(ctx.message, ctx.bot.owner))
+    return commands.check(lambda ctx: is_owner_check(ctx))
