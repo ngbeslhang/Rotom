5c0c6ff1e36912e8592cba2b22dbafc927f0a8f7
ngbeslhang <ngbeslhang@gmail.com>
2017-03-28 03:43:58 +0800

    Added ban command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,2 +1,12 @@
 """Admin cog for Rotom"""
 from discord.ext import commands
+from utils import checks
+
+class Admin:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["hammer"], pass_context=True)
+    @checks.is_admin()
+    async def ban(self, ctx):
+        pass
