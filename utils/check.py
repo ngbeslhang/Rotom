ec72a010a409994dec9f0932177f5d943b4639a6
ngbeslhang <ngbeslhang@gmail.com>
2017-03-28 03:46:47 +0800

    Why do we have this anyway?

--- utils/check.py
+++
@@ -1,7 +0,0 @@
-# role check (i.e. user must have at least one of the roles
-#             specified in the database to use the command)
-
-# owner check (i.e. user's ID must be in config.yaml to use the command)
-
-# language check (i.e. check user's language choice then use the
-#                 cooresponding language pack)
