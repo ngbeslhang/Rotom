597bc1578e35ca219d82572fbcd680126ce4cd6a
ngbeslhang <ngbeslhang@gmail.com>
2017-03-28 02:47:51 +0800

    Why not combine server owner/selfbot check into one?

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -30,11 +30,8 @@ def is_superuser():
 # Admin check
 def is_admin_check(ctx):
     # Always true no matter what, that way database also don't need to record server owner's ID
-    if ctx.message.author.id is ctx.message.server.owner.id:
-        return True
-    
-    # If the bot is run in selfbot mode, return True since if they can't ban they lack permission to anyways
-    if ctx.bot.is_bot is False:
+    # Also, if the bot is run in selfbot mode, return True since if they can't ban they lack permission to anyways
+    if ctx.message.author.id is ctx.message.server.owner.id or ctx.bot.is_bot is False:
         return True
 
     if ctx.bot.db is None:
