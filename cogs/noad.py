974eee05304583f08fdd1ed2bbb80c3a3a12d470
ngbeslhang <ngbeslhang@gmail.com>
2017-04-10 03:49:25 +0800

    fuck this

--- cogs/noad.py
+++ cogs/noad.py
@@ -14,28 +14,29 @@ class NoAd:
                 if "discord.gg/" in msg.content:
                     if "discord.gg/pokemon" not in msg.content:
                         # 270033143996612608
-                        if [r.id for r in msg.author.roles] not in roles or msg.channel.id != '180261884979576832':
-                            await self.bot.delete_message(msg)
-                            await self.bot.send_message(msg.channel, 
-                                "{0} Your message has been deleted because: Your message contains an invite link.".format(msg.author.mention))
+                        if [r.id for r in msg.author.roles] not in roles:
+                            if msg.channel.id != '180261884979576832'
+                                await self.bot.delete_message(msg)
+                                await self.bot.send_message(msg.channel, 
+                                    "{0} Your message has been deleted because: Your message contains an invite link.".format(msg.author.mention))
 
-                            try:
-                                em = discord.Embed(title="SOMEONE POSTED AN INVITE")
-                                em.add_field(name="Content", value=msg.content, inline=False)
-                                em.add_field(name="Message ID", value=msg.id)
-                                em.add_field(name="Posted at", value=msg.channel.name)
+                                try:
+                                    em = discord.Embed(title="SOMEONE POSTED AN INVITE")
+                                    em.add_field(name="Content", value=msg.content, inline=False)
+                                    em.add_field(name="Message ID", value=msg.id)
+                                    em.add_field(name="Posted at", value=msg.channel.name)
 
-                                await self.bot.send_message(
-                                    msg.server.get_channel('272438017745223681'),
-                                    "{0.name} `{0.id}`".format(msg.author),
-                                    embed=em
-                                )
-                            except (Forbidden, NotFound, InvalidArgument):
-                                self.bot.log.warning("[NoAd] Channel not found!")
+                                    await self.bot.send_message(
+                                        msg.server.get_channel('272438017745223681'),
+                                        "{0.name} `{0.id}`".format(msg.author),
+                                        embed=em
+                                    )
+                                except (Forbidden, NotFound, InvalidArgument):
+                                    self.bot.log.warning("[NoAd] Channel not found!")
                                 
-                            self.bot.log.info("[NoAd] User: {0.name} ({0.id})".format(msg.author))
-                            self.bot.log.info("[NoAd] Content: {0.content}".format(msg))
-                            self.bot.log.info("[NoAd] At: {0.name} ({0.id})".format(msg.channel))
+                                self.bot.log.info("[NoAd] User: {0.name} ({0.id})".format(msg.author))
+                                self.bot.log.info("[NoAd] Content: {0.content}".format(msg))
+                                self.bot.log.info("[NoAd] At: {0.name} ({0.id})".format(msg.channel))
                                 
 
 def setup(bot):
