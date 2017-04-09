0e887e54fe26545fe050e47368536116f996d482
ngbeslhang <ngbeslhang@gmail.com>
2017-04-10 04:12:08 +0800

    fuck you too ext

--- cogs/noad.py
+++ cogs/noad.py
@@ -16,8 +16,8 @@ class NoAd:
                     # 270033143996612608
                     if msg.channel.id != '180261884979576832':
                         if any(e in [r.id for r in msg.author.roles] for e in roles) is False:
-                            self.bot.say("```{}```".format([r.id for r in msg.author.roles]))
-                            self.bot.say("```{}```".format(roles))
+                            self.bot.send_message(msg.channel, "```{}```".format([r.id for r in msg.author.roles]))
+                            self.bot.send_message(msg.channel, "```{}```".format(roles))
                             await self.bot.delete_message(msg)
                             await self.bot.send_message(msg.channel, 
                                 "{0} Your message has been deleted because: Your message contains an invite link.".format(msg.author.mention))
