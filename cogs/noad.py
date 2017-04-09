a923bc523b0dc6521fedbaf8a7d4cfaa72206438
ngbeslhang <ngbeslhang@gmail.com>
2017-04-10 05:09:48 +0800

    Cmon just work

--- cogs/noad.py
+++ cogs/noad.py
@@ -19,7 +19,8 @@ class NoAd:
                                 "{0} Your message has been deleted because: Your message contains an invite link.".format(msg.author.mention))
 
                             try:
-                                em = discord.Embed(colour=discord.Colour().orange())
+                                em = discord.Embed()
+                                em.colour = discord.Colour().orange()
 
                                 if msg.author.avatar_url is not None:
                                     em.set_author(name=str(msg.author), icon_url=msg.author.avatar_url)
