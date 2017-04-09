eb19be0f0f5c7f28732cd14a4e59d5b505c2bfd9
ngbeslhang <ngbeslhang@gmail.com>
2017-04-10 04:06:57 +0800

    I have never felt so stupid

--- cogs/noad.py
+++ cogs/noad.py
@@ -15,7 +15,7 @@ class NoAd:
                 if "discord.gg/pokemon" not in msg.content:
                     # 270033143996612608
                     if msg.channel.id != '180261884979576832':
-                        if any(e in [r.id for r in msg.author.roles] for e in roles) is False:
+                        if not set([r.id for r in msg.author.roles]).intersection(roles):
                             await self.bot.delete_message(msg)
                             await self.bot.send_message(msg.channel, 
                                 "{0} Your message has been deleted because: Your message contains an invite link.".format(msg.author.mention))
