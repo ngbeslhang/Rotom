5c43ad15a1b9e050bd2bd06e60f97420d56c4c08
ngbeslhang <ngbeslhang@gmail.com>
2017-05-12 13:16:42 +0800

    Wrote init

--- cogs/db_rethinkdb.py
+++ cogs/db_rethinkdb.py
@@ -1,8 +1,16 @@
 import rethinkdb as r
+r.set_loop_type('asyncio')
 
 class DB:
     def __init__(self, bot):
-        pass
+        self.bot = bot
+        bot.loop.run_until_complete(self._init())
+
+    async def _init(self):
+        try:
+            self._conn = r.connect()
+        except r.ReqlDriverError:
+            self.bot.log.error("[RethinkDB] Unable to connect to database!")
 
 def setup(bot):
     bot.add_cog(DB(bot))
