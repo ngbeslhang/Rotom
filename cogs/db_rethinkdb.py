1309c93192a3770998270ad2f5d6c7ecae024e17
ngbeslhang <ngbeslhang@gmail.com>
2017-05-17 22:22:42 +0800

    Finished the main init function

--- cogs/db_rethinkdb.py
+++ cogs/db_rethinkdb.py
@@ -7,10 +7,20 @@ class DB:
         bot.loop.run_until_complete(self._init())
 
     async def _init(self):
+        conf = self.bot.get_conf()
         try:
-            self._conn = r.connect()
-        except r.ReqlDriverError:
-            self.bot.log.error("[RethinkDB] Unable to connect to database!")
+            self._conn = await r.connect(host=conf['host'], port=conf['port'], db=conf['name'])
+            self.bot.log.info("[RethinkDB] Successfully connected!")
+        except r.RqlDriverError:
+            try:
+                self.bot.log.info("[RethinkDB] Database not created yet, creating...")
+                self._conn = await r.connect(host=conf['host'], port=conf['port'])
+                r.db_create(conf['name'])
+                self.bot.log.info("[RethinkDB] Successfully connected and created database!")
+            except r.RqlDriverError:
+                self.bot.log.error("[RethinkDB] Unable to connect to database!")
+        except TypeError:
+            self.bot.log.error("[RethinkDB] Config does not exist!")
 
 def setup(bot):
     bot.add_cog(DB(bot))
