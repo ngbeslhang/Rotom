1f143da4c34d0c5ea54df365ffb844d0c638a37c
ngbeslhang <ngbeslhang@gmail.com>
2017-06-15 19:55:53 +0800

    I'm a fucking stupid

--- cogs/robodanny.py
+++ cogs/robodanny.py
@@ -111,5 +111,150 @@ class RoboDanny:
 
         return card, entries
 
+    def parse_google_card(self, node):
+        if node is None:
+            return None
+
+        e = discord.Embed(colour=0x738bd7)
+
+        # check if it's a calculator card:
+        calculator = node.find(".//table/tr/td/span[@class='nobr']/h2[@class='r']")
+        if calculator is not None:
+            e.title = 'Calculator'
+            e.description = ''.join(calculator.itertext())
+            return e
+
+        parent = node.getparent()
+
+        # check for unit conversion card
+        unit = parent.find(".//ol//div[@class='_Tsb']")
+        if unit is not None:
+            e.title = 'Unit Conversion'
+            e.description = ''.join(''.join(n.itertext()) for n in unit)
+            return e
+
+        # check for currency conversion card
+        currency = parent.find(".//ol/table[@class='std _tLi']/tr/td/h2")
+        if currency is not None:
+            e.title = 'Currency Conversion'
+            e.description = ''.join(currency.itertext())
+            return e
+
+        # check for release date card
+        release = parent.find(".//div[@id='_vBb']")
+        if release is not None:
+            try:
+                e.description = ''.join(release[0].itertext()).strip()
+                e.title = ''.join(release[1].itertext()).strip()
+                return e
+            except:
+                return None
+
+        # check for definition card
+        words = parent.find(".//ol/div[@class='g']/div/h3[@class='r']/div")
+        if words is not None:
+            try:
+                definition_info = words.getparent().getparent()[1] # yikes
+            except:
+                pass
+            else:
+                try:
+                    # inside is a <div> with two <span>
+                    # the first is the actual word, the second is the pronunciation
+                    e.title = words[0].text
+                    e.description = words[1].text
+                except:
+                    return None
+
+                # inside the table there's the actual definitions
+                # they're separated as noun/verb/adjective with a list
+                # of definitions
+                for row in definition_info:
+                    if len(row.attrib) != 0:
+                        # definitions are empty <tr>
+                        # if there is something in the <tr> then we're done
+                        # with the definitions
+                        break
+
+                    try:
+                        data = row[0]
+                        lexical_category = data[0].text
+                        body = []
+                        for index, definition in enumerate(data[1], 1):
+                            body.append('%s. %s' % (index, definition.text))
+
+                        e.add_field(name=lexical_category, value='\n'.join(body), inline=False)
+                    except:
+                        continue
+
+                return e
+
+        # check for "time in" card
+        time_in = parent.find(".//ol//div[@class='_Tsb _HOb _Qeb']")
+        if time_in is not None:
+            try:
+                time_place = ''.join(time_in.find("span[@class='_HOb _Qeb']").itertext()).strip()
+                the_time = ''.join(time_in.find("div[@class='_rkc _Peb']").itertext()).strip()
+                the_date = ''.join(time_in.find("div[@class='_HOb _Qeb']").itertext()).strip()
+            except:
+                return None
+            else:
+                e.title = time_place
+                e.description = '%s\n%s' % (the_time, the_date)
+                return e
+
+        # check for weather card
+        # this one is the most complicated of the group lol
+        # everything is under a <div class="e"> which has a
+        # <h3>{{ weather for place }}</h3>
+        # string, the rest is fucking table fuckery.
+        weather = parent.find(".//ol//div[@class='e']")
+        if weather is None:
+            return None
+
+        location = weather.find('h3')
+        if location is None:
+            return None
+
+        e.title = ''.join(location.itertext())
+
+        table = weather.find('table')
+        if table is None:
+            return None
+
+        # This is gonna be a bit fucky.
+        # So the part we care about is on the second data
+        # column of the first tr
+        try:
+            tr = table[0]
+            img = tr[0].find('img')
+            category = img.get('alt')
+            image = 'https:' + img.get('src')
+            temperature = tr[1].xpath("./span[@class='wob_t']//text()")[0]
+        except:
+            return None # RIP
+        else:
+            e.set_thumbnail(url=image)
+            e.description = '*%s*' % category
+            e.add_field(name='Temperature', value=temperature)
+
+        # On the 4th column it tells us our wind speeds
+        try:
+            wind = ''.join(table[3].itertext()).replace('Wind: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Wind', value=wind)
+
+        # On the 5th column it tells us our humidity
+        try:
+            humidity = ''.join(table[4][0].itertext()).replace('Humidity: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Humidity', value=humidity)
+
+        return e
+
 def setup(bot):
     bot.add_cog(RoboDanny(bot))
