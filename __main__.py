77fa43a99a753495964afe3623e9ba9187e6ac16
ngbeslhang <ngbeslhang@gmail.com>
2017-06-22 04:12:34 +0800

    I'm an idiot

---
+++ __main__.py
@@ -0,0 +1,11 @@
+from .rotom import Bot
+import argparse
+
+parser = argparse.ArgumentParser(description='Runs Rotom.')
+parser.add_argument('-c', '--config', type=str, help='Config file name (default: config.yml)')
+parser.add_argument('-d', '--debug', help='Enable debug mode', action='store_true')
+args = parser.parse_args()
+if args.config is None:
+    args.config = 'config.yml'
+rotom = Bot(config=args.config, debug=args.debug)
+    rotom.run()
