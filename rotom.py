aa720fef5c9d14a50019875b6fae97ec14ee5422
ngbeslhang <ngbeslhang@gmail.com>
2017-06-22 04:08:20 +0800

    Moved the __main__ check to __init__.py

--- rotom.py
+++ rotom.py
@@ -219,15 +219,3 @@ class Builtin:
             await ctx.message.edit(python.format(code, result))
         else:
             await ctx.message.channel.send(python.format(code, result))
-
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description='Runs Rotom.')
-    parser.add_argument('-c', '--config', type=str, help='Config file name (default: config.yml)')
-    parser.add_argument('-d', '--debug', help='Enable debug mode', action='store_true')
-    args = parser.parse_args()
-    if args.config is None:
-        args.config = 'config.yml'
-    rotom = Bot(config=args.config, debug=args.debug)
-    rotom.run()
