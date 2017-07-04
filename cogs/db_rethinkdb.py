194c0ce244c615f211c7ffa33a349f5ed9343259
ngbeslhang <ngbeslhang@gmail.com>
2017-07-04 18:06:52 +0800

    The database cog should be entirely completed

--- cogs/db_rethinkdb.py
+++ cogs/db_rethinkdb.py
@@ -1,6 +1,7 @@
 import rethinkdb as r
 r.set_loop_type('asyncio')
 
+
 class DB:
     def __init__(self, bot):
         self.bot = bot
@@ -22,7 +23,11 @@ class DB:
         temp = self._conf
         name = temp['name']
         del temp['name']
-        conn = await r.connect(**temp)
+        try:
+            conn = await r.connect(**temp)
+        except r.RqlDriverError:
+            self.bot.log.error("[RethinkDB] Unable to connect to database!")
+            return None
 
         try:
             await conn.use(name)
@@ -46,40 +51,114 @@ class DB:
         Exceptions:
         **NOTE**: Only built-in exceptions will be used for maximum modularity.
         `NameError` - Given table does not exist.
-        `KeyError` - Given key does not exist."""
+        `KeyError`  - Given key does not exist.
+        `TypeError` - Passed document ID is not in either str or int type."""
         conn = await self._connect()
-        t = await r.table(tbl)
 
         try:
-            await t.run(conn)
-        except r.errors.ReqlOpFailedError:
-            raise NameError("Table {} does not exist.".format(tbl))
-        
-        if type(name) is int:
-            name = str(name)
+            t = await r.table(tbl)
 
-        result = await t.get(name).run(conn)
+            try:
+                await t.run(conn)
+            except r.errors.ReqlOpFailedError:
+                raise NameError("Table {} does not exist.".format(tbl))
 
-        if result is not None:
-            return result
-        else:
-            raise KeyError("Key {} does not exist inside table {}.".format(name, tbl))
+            if type(name) is int:
+                name = str(name)
+            elif type(name) is not str:
+                raise TypeError("Document ID must be either str or int.")
 
-    async def write(self, tbl: str, name, data: dict, conflict="update"):
+            result = await t.get(name).run(conn)
+
+            if result is not None:
+                return result
+            else:
+                raise KeyError("Key {} does not exist inside table {}.".format(name, tbl))
+        finally:
+            if conn:
+                await conn.close()
+
+    async def write(self, tbl: str, data: dict, conflict="update"):
         """Insert document into given table with given ID.
         If given table doesn't exists a new table with the given name will be made and the document with given ID will be inserted.
         If a document with the given name inside the table already exists it will be updated instead by default.
 
-        tbl : str    - Table name.
-        name         - Document ID. If int is passed, it will be converted to str.
+        tbl   : str  - Table name.
         data  : dict - Document data.
-        conflict     - Ctrl+F `conflict` at https://www.rethinkdb.com/api/python/insert/"""
+        conflict     - Ctrl+F `conflict` at https://www.rethinkdb.com/api/python/insert/
+
+        Exceptions:
+        **NOTE**: Only built-in exceptions will be used for maximum modularity.
+        `TypeError` - Passed document ID is not in either str or int type.
+
+        **NOTE**: In order to include document ID, please insert `"id": "name"` into `data`.
+        The `id` value will be type-checked and converted accordingly with only str and int supported.
+        If it's not inncluded, RethinkDB will assign a random ID instead.
+        
+        **NOTE**: **kwargs support is planned."""
+        conn = await self._connect()
+
+        try:
+            t = await r.table(tbl)
+
+            try:
+                await t.run(conn)
+            except r.errors.ReqlOpFailedError:
+                self.bot.log.info(
+                    "[RethinkDB] Table {} does not exist yet, creating...".format(tbl))
+                await r.table_create(tbl).run(conn)
+                self.bot.log.info("[RethinkDB] Table {} successfully created!".format(tbl))
+            
+            try:
+                name = data['id']
+                if type(name) is int:
+                    data['id'] = str(name)
+                elif type(name) is not str:
+                    raise TypeError("Document ID must be either str or int.")
+            except KeyError:
+                pass # Since it doesn't quite matter for us to default to randomly assigned ID 
+            
+            try:
+                await t.insert(data, conflict=conflict).run(conn)
+            except r.errors.ReqlOpFailedError as e:
+                raise RuntimeError("Unable to insert value, reason: {}".format(e))
+        finally:
+            if conn:
+                await conn.close()
 
     async def delete(self, tbl: str, name):
         """Delete document with given ID from the given table.
 
         tbl : str - Table name.
-        name      - Document ID. If int is passed, it will be converted to str."""
+        name      - Document ID. If int is passed, it will be converted to str.
+
+        Exceptions:
+        **NOTE**: Only built-in exceptions will be used for maximum modularity.
+        `NameError` - Given table does not exist.
+        `KeyError`  - Given key does not exist.
+        `TypeError` - Passed document ID is not in either str or int type."""
+        conn = await self._connect()
+
+        try:
+            t = await r.table(tbl)
+
+            try:
+                await t.run(conn)
+            except r.errors.ReqlOpFailedError:
+                raise NameError("Table {} does not exist.".format(tbl))
+
+            if type(name) is int:
+                name = str(name)
+            elif type(name) is not str:
+                raise TypeError("Document ID must be either str or int.")
+            
+            try:
+                await t.get(name).delete().run(conn)
+            except r.errors.ReqlOpFailedError as e:
+                raise KeyError("Unable to delete key {} inside table {}, reason: {}.".format(name, tbl, e))
+        finally:
+            if conn:
+                await conn.close()        
 
     async def _eval(self, code: str):
         """Evaluate database code with the built-in eval command and returns the result.
