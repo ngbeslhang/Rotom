93253b4f561bd8f747572d18867dcb793f057264
ngbeslhang <ngbeslhang@gmail.com>
2017-07-12 20:51:12 +0800

    Updated to add beginner setup support

---
+++ configure.py
@@ -0,0 +1,7 @@
+def setup():
+    """Easy setup for beginners so that I hope they won't bother me for bot setup-related questions"""
+    print("The setup is a WIP, come back later.")
+    return
+
+    # The actual code down here
+    
