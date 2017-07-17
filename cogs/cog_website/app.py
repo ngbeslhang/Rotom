2927211e08a646100b804084b0076f77ad66d580
ngbeslhang <ngbeslhang@gmail.com>
2017-07-18 03:00:09 +0800

    Fuck you .gitignore

--- cogs/cog_website/app.py
+++ cogs/cog_website/app.py
@@ -1,9 +1,20 @@
-"""Directly from the example of Kyoukai's GitHub repo README.md, solely for testing right now."""
-import json
-from kyoukai import Kyoukai, HTTPRequestContext
+"""Partially based on the example of Kyoukai's GitHub repo README.md, solely for testing right now."""
+import kyoukai
 
-kyk = Kyoukai("example_app")
+blueprint = kyoukai.Blueprint("blueprint")
 
-@kyk.route("/")
-async def index(ctx: HTTPRequestContext):
-    return json.dumps(dict(ctx.request.headers)), 200, {"Content-Type": "application/json"}
+
+class Site(kyoukai.Kyoukai):
+    def __init__(self, bot, name: str="website_cog", ip: str="127.0.0.1", port: int=4444):
+        super().__init__(name)
+        self.register_blueprint(blueprint)
+
+    @blueprint.route("/")
+    async def index(ctx: kyoukai.HTTPRequestContext):
+        return "Hi"
+
+
+# Made for internal testing.
+if __name__ == "__main__":
+    # Port 22 confirmed in use on my Windows PC
+    Site("placeholder").run()
