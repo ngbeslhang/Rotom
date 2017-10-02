319abeefd27527370acb819fcc39535e258e9410
ngbeslhang <ngbeslhang@gmail.com>
2017-10-02 16:07:28 +0800

    Lots of stuffs

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Rotom
-**WARNING:** **DO NOT** remove `config.yaml` from .gitignore UNLESS you know what are you doing.
-
 **NOTE:** This repo is still WIP.
 
 # Requirement
@@ -9,3 +7,66 @@
 
 # Installation
 WIP
+
+# Configuration
+A template config file is provided for you to start with.
+**WARNING:** You are expected to have a minimal basic understanding of Python or programming in general.
+
+```yaml
+# If unsure, Google the term + "YAML" without quotes
+---
+bot:
+  # Your bot's token
+  token: quakquakquak
+
+  # Your bot's prefix
+  prefix: r.
+  # Supports either a string like the one above or a list/array/collection:
+  #   prefix: 
+  #     - r.
+  #     - bzzt
+
+  # Bypass all bots' messages so they won't be able to used this bot's commands
+  bypass_bot_messages: yes/no
+
+  plugins:
+    # Directory of plugins, both relative and absolute are supported
+    dir: plugins
+
+    # Choose what plugins to load while the bot is starting
+    load: ~
+    # Enter ~ to disable plugin loading, otherwise the same as prefix config
+
+  # Read the parameters section of the link below...
+  # http://discordpy.readthedocs.io/en/rewrite/api.html#discord.Client
+  # ...and check everything ABOVE the add_check() for a complete list of params:
+  # http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html#discord.ext.commands.Bot.add_check
+  # (ignore all command_prefix and formatter)
+  params: ~
+  # When needed, do it like this:
+  #   params:
+  #     description: An ever evolving bot.
+  #     self_bot: no
+```
+
+If config for the plugins are needed, put the configs right under the `bot` and so on:
+```yaml
+---
+bot:
+  token: TOKEN
+  prefix: r.
+  bypass_bot_messages: yes
+
+  plugins:
+    dir: plugins
+    load: adblock
+
+  params: ~
+
+# Below is a plugin config example
+adblock:
+    block: 
+      - discord.gg/*
+    bypass:
+      - discord.gg/pokemon
+````
--- bot.py
+++ bot.py
@@ -1,11 +1,128 @@
 """Rotom's core"""
+import sys
+import inspect
+import importlib
 import asyncio
 import discord
 
 class Bot(discord.ext.commands.AutoShardedBot):
-    def __init__(self, conf, debug):
+    def __init__(self, **kwargs):
         # Initializations
-        self.boot_time = __import__('time').time()
+        import time
+        self.boot_time = time.time()
 
-    # TODO: Modify the extension import/export functions
-    # TODO: Write a custom command class?
+    def _init_log(self, conf_name, debug):
+        """Initialize logging."""
+        import datetime, os, logging
+        # Credits to Liara: https://github.com/Thessia/Liara/blob/master/liara.py#L83
+        now = str(datetime.datetime.now()).replace(' ', '_').replace(':', '-').split('.')[0]
+        formatter = logging.Formatter(
+            fmt='%(asctime)s [%(levelname)s] %(message)s', datefmt='GMT%z %Y-%m-%d %I:%M:%S %p')
+
+        self.log = logging.getLogger('rotom')
+        if debug:
+            self.log.setLevel(logging.DEBUG)
+        else:
+            self.log.setLevel(logging.INFO)
+
+        # Creates a log folder if it doesn't exist just in case
+        if not os.path.exists("logs/"):
+            os.makedirs("logs/")
+
+        if not os.path.exists("logs/rotom"):
+            os.makedirs("logs/rotom")
+
+        handler = logging.FileHandler('logs/rotom/rotom-{}_{}.log'.format(conf_name, now))
+        handler.setFormatter(formatter)
+        self.log.addHandler(handler)
+
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
+        self.log.addHandler(handler)
+        self.log.info("Successfully set up logging!")
+
+        self.discord_log = logging.getLogger('discord')
+        self.discord_log.setLevel(logging.INFO)
+
+        if not os.path.exists("logs/discord"):
+            os.makedirs("logs/discord")
+
+        handler = logging.FileHandler('logs/discord/discord-{}_{}.log'.format(config, now))
+        handler.setFormatter(formatter)
+        self.discord_log.addHandler(handler)
+        self.log.info("Successfully set up discord.py logging!")
+
+    async def on_bot_init(self):
+        """Special event for listeners at bot initialization"""
+        pass
+
+    def add_cog(self, cog):
+        """Modified the original add_cog() for dependancy support.
+
+        Parameters
+        ----------
+        cog
+            The cog to register to the bot.
+        """
+        # TODO: Add dependancy support
+        self.cogs[type(cog).__name__] = cog
+
+        try:
+            check = getattr(cog, '_{.__class__.__name__}__global_check'.format(cog))
+        except AttributeError:
+            pass
+        else:
+            self.add_check(check)
+
+        try:
+            check = getattr(cog, '_{.__class__.__name__}__global_check_once'.format(cog))
+        except AttributeError:
+            pass
+        else:
+            self.add_check(check, call_once=True)
+
+        members = inspect.getmembers(cog)
+        for name, member in members:
+            # register commands the cog has
+            if isinstance(member, Command):
+                if member.parent is None:
+                    self.add_command(member)
+                continue
+
+            # register event listeners the cog has
+            if name.startswith('on_'):
+                self.add_listener(member, name)
+
+
+    def load_estension(self, name):
+        """Loads an extension.
+        An extension is a python module that contains commands, cogs, or
+        listeners.
+        An extension must have a global function, ``setup`` defined as
+        the entry point on what to do when the extension is loaded. This entry
+        point must have a single argument, the ``bot``.
+        Parameters
+        ------------
+        name: str
+            The extension name to load. It must be dot separated like
+            regular Python imports if accessing a sub-module. e.g.
+            ``foo.test`` if you want to import ``foo/test.py``.
+        Raises
+        --------
+        ClientException
+            The extension does not have a setup function.
+        ImportError
+            The extension could not be imported.
+        """
+
+        if name in self.extensions:
+            return
+
+        lib = importlib.import_module(name)
+        if not hasattr(lib, 'setup'):
+            del lib
+            del sys.modules[name]
+            raise discord.ClientException('extension does not have a setup function')
+
+        lib.setup(self)
+        self.extensions[name] = lib
